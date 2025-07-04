f67b19523deb6fff05870f465fbebd2869606b2d
thegr8lewis <lewis.nyakaru@gmail.com>
2025-07-04 21:18:25 +0300

    refactor: Update CORS_ALLOWED_ORIGINS to use HTTPS for improved security

--- report_backend/report_backend/deployment_settings.py
+++ report_backend/report_backend/deployment_settings.py
@@ -23,7 +23,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    'http://safespacekenya.onrender.com' 
+    'https://safespacekenya.onrender.com' 
 ]
 
 # ALLOWED_HOSTS = [ 'http://safespacekenya.onrender.com' ]
