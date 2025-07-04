74a82c28c094f8d6dbf702ca51ecd7b0cdfb9e51
thegr8lewis <lewis.nyakaru@gmail.com>
2025-07-04 14:26:33 +0300

    refactor: Clean up build script by commenting out unused commands

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
+# set -o errexit
+
+# pip install -r requirements.txt
+# python manage.py collectstatic --noinput
+# python manage.py migrate
+
+
+#!/usr/bin/env bash
 set -o errexit
 
+# Move to the directory containing this script (report_backend/)
+cd "$(dirname "$0")"
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Run Django commands using the correct path
 python manage.py collectstatic --noinput
 python manage.py migrate
