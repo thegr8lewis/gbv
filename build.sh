64b23e26ab1a46eff0a5325224fceb0b682bd233
thegr8lewis <lewis.nyakaru@gmail.com>
2025-07-04 21:39:23 +0300

    refactor: Clean up build script by removing commented-out code and ensuring proper structure

--- build.sh
+++ build.sh
@@ -1,14 +1,3 @@
-# #!/usr/bin/env bash
-# set -o errexit
-
-# # Move into the Django backend folder
-# cd report_backend
-
-# pip install -r ../requirements.txt
-# python manage.py collectstatic --noinput
-# python manage.py migrate
-
-
 #!/usr/bin/env bash
 # Exit on error
 set -o errexit
@@ -24,3 +13,8 @@ python manage.py collectstatic --noinput
 
 # Run migrations
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]]
+then
+    python manage.py createsuperuser --no-input
+fi
