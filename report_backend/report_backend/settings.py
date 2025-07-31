6b2de5b2b8fa539edc654633b6185d9defe7f0e0
thegr8lewis <lewis.nyakaru@gmail.com>
2025-07-31 19:03:55 +0300

    434y4u3654

--- report_backend/report_backend/settings.py
+++ report_backend/report_backend/settings.py
@@ -80,7 +80,7 @@ REST_FRAMEWORK = {
 
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    # 'http://localhost:5173',
      'https://gbv4.onrender.com',
      'https://safespacekenya.onrender.com'
 ]
@@ -127,27 +127,27 @@ WSGI_APPLICATION = 'report_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Changed from mysql
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),  # Changed default from 3306 to 5432
-        'OPTIONS': {
-            # PostgreSQL specific options
-            'client_encoding': 'UTF8',
-            'connect_timeout': 5,
-        }
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get("DATABASE_URL")
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',  # Changed from mysql
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT', '5432'),  # Changed default from 3306 to 5432
+#         'OPTIONS': {
+#             # PostgreSQL specific options
+#             'client_encoding': 'UTF8',
+#             'connect_timeout': 5,
+#         }
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
